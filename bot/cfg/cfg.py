469ec38264a7edb74c831228395697dbaed50944
Trimatix <1jasperlaw@gmail.com>
2021-01-11 19:57:12 +0000

    fix conflicts

--- bot/cfg/cfg.py
+++ bot/cfg/cfg.py
@@ -81,5 +81,7 @@ developers = [188618589102669826]
 botToken = "STORYTELLER_BOT_TOKEN"
 botToken_envVarName = ""
 
+ignoredSymbols = ".,!?`%&()[]-+=*|\/<>¬~'\"_\n"
+
 pollMenuResultsBarLength = 10
 pollMenuDefaultTimeout = {"minutes": 5}
